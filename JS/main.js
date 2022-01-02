246867b406cd33c79d3d1e4f340dbddef36298ec
unknown <kokosadek55@gmail.com>
2022-01-03 01:42:08 +0200

    First Realese

---
+++ JS/main.js
@@ -0,0 +1,44 @@
+
+    //Select services Selector
+
+    let ourservices = document.querySelector(".services");
+
+    window.onscroll = () => {
+
+        //services Offset Top
+    let ourservicesOffsetTop = ourservices.offsetTop;
+        
+        //Outer Height
+    let ourservicesOuterHeight = ourservices.offsetHeight;
+    
+        //Window Height
+    let windowHeight = this.innerHeight;
+
+        //windowScrollTop
+    let windowScrollTop = this.pageYOffset;
+        
+    if (windowScrollTop > ((ourservicesOffsetTop + ourservicesOuterHeight) - windowHeight)) {
+        
+        let allservices = document.querySelectorAll(".service-box .service-progress span");
+
+        allservices.forEach(service => {
+
+            service.style.width = service.dataset.progress;
+
+        });
+    }
+};
+
+    // Start Bullets
+
+    let ourBullets = document.querySelectorAll(".nav-bullets .bullet");
+
+    ourBullets.forEach(bullet => {
+
+        bullet.addEventListener('click' , (e) =>{
+
+            document.querySelector(e.target.dataset.section).scrollIntoView({behavior:'smooth'});
+        });
+
+    });
+    
